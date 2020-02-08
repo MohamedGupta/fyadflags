853c8dc65be714e2d619372768eaa5629b66e2ad
Fucktard Idiot <info@fucktardidiot.com>
2020-02-08 08:23:39 -0600

    Fixed it

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 cred = twitter_config.accounts['FYADFlags']
 s3 = boto3.resource("s3")
 FLAG_URL = 'http://forums.somethingawful.com/flag.php?forumid=26'
-BUCKET = "fucktardidiot"
+BUCKET = "theBucket"
 STATE = s3.Object(BUCKET, "fyadflags/index.json")
 twitter_api = twitter.Api(consumer_key=cred['consumer_key'], consumer_secret=cred['consumer_secret'],
                             access_token_key=cred['access_token_key'], access_token_secret=cred['access_token_secret'])
@@ -23,7 +23,7 @@ def get_state():
 
 
 def handler(event, context):
-    tweet_flag(twitter_api)
+    tweet_flag()
 
 
 def flag_already_saved(flag_content):
@@ -46,24 +46,25 @@ def archive_handler(event, context):
     tweet_flag(True)
 
 
-def tweet_flag(archive_only=False):
+def tweet_flag():
     flag = requests.get(FLAG_URL).json()
     while flag['username'] in twitter_config.blacklist_flags or flag['path'] in twitter_config.blacklist_flagpaths:
         flag = requests.get(FLAG_URL).json()
 
     flagpath = "http://fi.somethingawful.com/flags" + flag['path']
     flag_description = f"#FYADflag by {flag['username']} {flag['created']}"
+    print(f'Posting flag: {flag_description}')
     if flag['path'] != 'error.png':
         try:
             if not archive_only:
-                r = twitter_api.PostUpdate(mediatweet, media=flagpath)
+                r = twitter_api.PostUpdate(flag_description, media=flagpath)
                 print('Posted flag ' + flagpath)
             flag_content = requests.get(flagpath).content
             flag['flag_key'] = get_flag_key(flag_description, flag_content)
             if not flag_already_saved(flag_content):
-                s3flag = s3.Object(BUCKET, get_flag_key(flag_description, flag_content))
-                s3flag.put(Body=flag_content)
-                record_flag(flag_content, flag)
+               s3flag = s3.Object(BUCKET, get_flag_key(flag_description, flag_content))
+               s3flag.put(Body=flag_content)
+               record_flag(flag_content, flag)
 
         except:
             raise
@@ -81,4 +82,5 @@ if __name__ == '__main__':
         exit()
 
     tweet_flag(api)
+
 
