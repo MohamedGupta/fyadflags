74555fe0efd61e913b085d33f054153129071807
Fucktard Idiot <info@fucktardidiot.com>
2020-02-08 07:57:26 -0600

    :wip: archive flags to s3. doesn't work (permissions fail)

--- main.py
+++ main.py
@@ -1,32 +1,72 @@
 #!/usr/bin/env python
 import twitter, twitter_config
 import requests
-#cred = next(acct for acct in twitter_config.accounts if acct['username'] == 'FYADFlags')
+import boto3
+import hashlib
+from collections import defaultdict
+
 cred = twitter_config.accounts['FYADFlags']
+s3 = boto3.resource("s3")
+FLAG_URL = 'http://forums.somethingawful.com/flag.php?forumid=26'
+BUCKET = "fucktardidiot"
+STATE = s3.Object(BUCKET, "fyadflags/index.json")
+twitter_api = twitter.Api(consumer_key=cred['consumer_key'], consumer_secret=cred['consumer_secret'],
+                            access_token_key=cred['access_token_key'], access_token_secret=cred['access_token_secret'])
+
+
+def get_state():
+    try:
+        return json.loads(STATE.get()['Body'])
+    except Exception as e:
+        print('Failed to load state: {e}')
+        return defaultdict(dict)
 
 
 def handler(event, context):
-    try:
-        api = twitter.Api(consumer_key=cred['consumer_key'], consumer_secret=cred['consumer_secret'],
-                           access_token_key=cred['access_token_key'], access_token_secret=cred['access_token_secret'])
-    except:
-        print('Failed to authenticate.')
-    tweet_flag(api)
+    tweet_flag(twitter_api)
 
 
-def tweet_flag(api):
-    flag = requests.get('http://forums.somethingawful.com/flag.php?forumid=26').json()
+def flag_already_saved(flag_content):
+    state = get_state()
+    checksum = hashlib.md5(flag_content).hexdigest()
+    return checksum in (state.get('flags') or [])
+
+
+def record_flag(flag, flag_content):
+    state = get_state()
+    checksum = hashlib.md5(flag_content).hexdigest()
+    state['flags']['checksum'] = flag
+
+
+def get_flag_key(flag_description, flag_content):
+    return f"fyadflags/{flag_description.replace(' ', '_')}_{hashlib.md5(flag_content).hexdigest()}"
+
+
+def archive_handler(event, context):
+    tweet_flag(True)
+
+
+def tweet_flag(archive_only=False):
+    flag = requests.get(FLAG_URL).json()
     while flag['username'] in twitter_config.blacklist_flags or flag['path'] in twitter_config.blacklist_flagpaths:
-        flag = requests.get('http://forums.somethingawful.com/flag.php?forumid=26').json()
-    #flag = json.loads(req.content)
+        flag = requests.get(FLAG_URL).json()
+
     flagpath = "http://fi.somethingawful.com/flags" + flag['path']
-    tweet = "#FYADflag " + flagpath + " by " + flag['username'] + " " + flag['created']
-    mediatweet = "#FYADflag by " + flag['username'] + " " + flag['created'] # + " " + flagpath
+    flag_description = f"#FYADflag by {flag['username']} {flag['created']}"
     if flag['path'] != 'error.png':
         try:
-            r = api.PostUpdate(mediatweet, media=flagpath)
-            print('Posted flag ' + flagpath)
+            if not archive_only:
+                r = twitter_api.PostUpdate(mediatweet, media=flagpath)
+                print('Posted flag ' + flagpath)
+            flag_content = requests.get(flagpath).content
+            flag['flag_key'] = get_flag_key(flag_description, flag_content)
+            if not flag_already_saved(flag_content):
+                s3flag = s3.Object(BUCKET, get_flag_key(flag_description, flag_content))
+                s3flag.put(Body=flag_content)
+                record_flag(flag_content, flag)
+
         except:
+            raise
             print('Flag post failed ' + flagpath)
     else:
         print('SA PHP error {0}'.format(flag))
